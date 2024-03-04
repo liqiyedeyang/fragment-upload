48fffbfde55cce8150f04b3a984e42657d28340f
lxy <lixiangyu1233@dingtalk.com>
2024-03-04 17:54:17 +0800

    feat：init project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <input type="file" @change="handleUploadChange" />
+</template>
+
+<script setup lang="ts">
+const handleUploadChange = (e: Event) => {
+  console.log(e.target.file);
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <input type="file" @change="handleUploadChange" />
+</template>
+
+<script setup lang="ts">
+const handleUploadChange = (e: Event) => {
+  console.log(e.target.file);
+};
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
